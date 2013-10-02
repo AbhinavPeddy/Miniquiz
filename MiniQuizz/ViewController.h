844e26333db08f54ced71d9e985813767c74fe66
thaher1505 <thaher1505@gmail.com>
2013-10-02 18:45:39 +0530

    Initial Commit

---
+++ MiniQuizz/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MiniQuizz
+//
+//  Created by CTSMacBook on 02/10/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MiniQuizz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MiniQuizz
+//
+//  Created by CTSMacBook on 02/10/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
